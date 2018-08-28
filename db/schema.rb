d2bfdc6f0c57ad46583d47a5e9ea16b87c6adcfa
“Andrew <andrewwoods88@gmail.com>
2018-08-28 15:21:30 -0400

    Review scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180822202614) do
+ActiveRecord::Schema.define(version: 20180828190959) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20180822202614) do
     t.bigint "image_file_size"
     t.datetime "image_updated_at"
     t.index ["user_id"], name: "index_movies_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
